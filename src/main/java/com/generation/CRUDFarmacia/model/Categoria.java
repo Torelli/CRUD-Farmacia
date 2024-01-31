ccb1b48df24541eb0c56c414e2bcce20429b5cb9
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-31 14:01:36 -0300

    Added Getter and Setter for Produto attribute

--- src/main/java/com/generation/CRUDFarmacia/model/Categoria.java
+++ src/main/java/com/generation/CRUDFarmacia/model/Categoria.java
@@ -37,4 +37,12 @@ public class Categoria {
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
 }
